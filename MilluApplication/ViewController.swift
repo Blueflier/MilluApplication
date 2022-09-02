0cb003472662d252615ee634f9c86d9866ef3a0a
Blueflier <joseph.e.hartono@gmail.com>
2022-09-02 13:27:29 -0700

    hello

--- MilluApplication/ViewController.swift
+++ MilluApplication/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  MilluApplication
+//  Millu
 //
-//  Created by Joseph Hartono on 9/2/22.
+//  Created by Joseph Hartono on 7/9/22.
 //
 
 import UIKit
@@ -12,8 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization (options: [.alert, .sound])
+        { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Reminder!"
+        content.body = "Ask your child the question of the day!"
+        // Step 3: Create the notification trigger
+        let date = Date().addingTimeInterval (10)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day,.hour, .minute, .second], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching:
+        dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier:
+        uuidString, content: content, trigger: trigger)
+        // Step 5: Register the request
+        center.add(request) { (error) in
+        // Check the error parameter and handle any errors
     }
-
-
+    }
 }
+    
+    
+    
 
