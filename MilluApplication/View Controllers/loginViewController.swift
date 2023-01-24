978a55fd3ee9e0090c3839c61d4c114c77462b31
ellieannar <ellieanna.ross@hotmail.com>
2023-01-24 14:36:28 -0800

    Began designing login screen

--- MilluApplication/View Controllers/loginViewController.swift
+++ MilluApplication/View Controllers/loginViewController.swift
@@ -11,8 +11,48 @@ class loginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //logo at top of screen
+    var logo: UIImageView {
+        let temp = UIImageView(image: UIImage(named: "solidLogo"))
+        temp.frame.origin = CGPoint(x: 0, y: 0)
+        temp.frame = CGRect(x: UIScreen.main.bounds.width/2-150, y: 100, width: 300, height: 150)
+        return temp
+    }
+    
+    //username text field
+    var userName: UITextField {
+        let temp = UITextField()
+        temp.placeholder = "Username"
+        temp.frame = CGRect(x: UIScreen.main.bounds.width/2-150, y: 300, width: 300, height: 30)
+        temp.textAlignment = .center
+        temp.borderStyle = UITextField.BorderStyle.roundedRect
+        temp.autocapitalizationType = .none
+        temp.autocorrectionType = .no
+        
+        return temp;
+    }
+    
+    //password text field
+    var password: UITextField {
+        let temp = UITextField()
+        temp.placeholder = "Password"
+        temp.frame = CGRect(x: UIScreen.main.bounds.width/2-150, y: 340, width: 300, height: 30)
+        temp.textAlignment = .center
+        temp.isSecureTextEntry = true
+        temp.autocapitalizationType = .none
+        temp.autocorrectionType = .no
+        temp.borderStyle = UITextField.BorderStyle.roundedRect
+        
+        return temp;
+    }
+    
+   
+    
+
     override func viewDidAppear(_ animated: Bool) {
-        
-        
+        view.addSubview(logo)
+        view.addSubview(userName)
+        view.addSubview(password)
     }
 }
